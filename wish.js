930bbc92441b97d5c4b8b92b552d8504c41c871d
syahruddinkamal-design <syahruddinkamal@gmail.com>
2026-05-12 13:22:14 +0700

    wish js add

---
+++ wish.js
@@ -0,0 +1,19 @@
+function kirimDanPindah() {
+    // 1. Mengambil elemen textarea berdasarkan ID 'wish'
+    const inputWish = document.getElementById('wish');
+    const isiWish = inputWish.value;
+
+    // 2. Cek apakah kolom harapan sudah diisi atau masih kosong
+    if (isiWish.trim() !== "") {
+        
+        // Menampilkan pesan doa (Pop-up)
+        alert("Amin ya Allah... Harapanmu sudah disimpan di hati. ✨");
+
+        // 3. Pindah ke halaman bunga.html
+        window.location.href = "bunga.html";
+        
+    } else {
+        // Jika Chelsea belum menulis apa-apa tapi sudah klik tombol
+        alert("Tuliskan dulu satu harapanmu di kolom ya, Chelsea.. :)");
+    }
+}
